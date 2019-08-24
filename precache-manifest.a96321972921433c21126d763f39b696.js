dca2af61ef0df36184fb386130151e9d36ce2689
PeterNelsonT <PeterNelsonT@.github.com>
2019-08-24 18:26:43 +0530

    Updates

---
+++ precache-manifest.a96321972921433c21126d763f39b696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bf890035e6f92f7ad8dd5a1e9043c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70a585b080db057adb50",
+    "url": "/monsters-rolodex/static/css/main.2d146514.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "70a585b080db057adb50",
+    "url": "/monsters-rolodex/static/js/main.c5df9fa3.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
